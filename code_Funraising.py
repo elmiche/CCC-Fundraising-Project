a4f3ccff5088340c4a39631201e9522a99c67153
MichéLozano <elmiche@latinooutdoors.org>
2019-11-26 21:38:36 -0800

    first commit

---
+++ code_Funraising.py
@@ -0,0 +1,9 @@
+
+import csv
+
+with open('/Users/ElMiche/Desktop/PDX_Code_Guild/Confidential_CCC_files/customer_listings_CCC.csv', 'r', encoding='ISO-8859-1') as file: 
+    data = file.read()
+
+print(data)
+
+# TODO save as dictionary
