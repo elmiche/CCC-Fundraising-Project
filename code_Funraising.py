e8f1597f0bc3b1e7da800d912ffd913e32bf10c9
MichéLozano <elmiche@latinooutdoors.org>
2020-04-06 16:47:22 -0700

    without pandas

--- code_Funraising.py
+++ code_Funraising.py
@@ -1,13 +1,6 @@
 import csv
 
-# import pandas as pd
-# from openpyxl.workbook import Workbook
 
-        #using pandas
-# df_csv = pd.read_csv('/Users/ElMiche/Desktop/PDX_Code_Guild/Confidential_CCC_files/customer_listings_CCC.csv')
-# print(df_csv)
-
-        #ORIGINAL READ CODE
 
 
 with open('/Users/ElMiche/Desktop/PDX_Code_Guild/Confidential_CCC_files/customer_listings_CCC.csv', 'r', encoding='ISO-8859-1') as file: 
