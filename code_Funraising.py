e29e457c45a4376ce03672686906f22bb70b323c
MichéLozano <elmiche@latinooutdoors.org>
2020-05-22 12:50:47 -0700

    ccc update

--- code_Funraising.py
+++ code_Funraising.py
@@ -1,19 +1,17 @@
 
 import csv
 
-
-# # THIS VERSION PRINTS THE LIST
+#    ------ THIS VERSION PRINTS THE LIST ------  ##
 # with open('/Users/ElMiche/Desktop/PDX_Code_Guild/Confidential_CCC_files/customer_listings_CCC.csv', 'r', encoding='ISO-8859-1') as file: 
 #     data = file.read()
- 
 
-# #variables
+
 customers = {}     
 
 # THIS VERSION DOESN'T PRINT YET
 
 with open('/Users/ElMiche/Desktop/PDX_Code_Guild/Confidential_CCC_files/customer_listings_CCC.csv', 'r', encoding='ISO-8859-1') as csv_file: 
-	data = csv.reader(csv_file, delimiter=',', header = True)
+	data = csv.reader(csv_file, delimiter=',')
 	line_count = 0
 	for contact_info in data:
 		#Skips header
@@ -31,18 +29,7 @@ with open('/Users/ElMiche/Desktop/PDX_Code_Guild/Confidential_CCC_files/customer
 			address = contact_info[3] #do last
 			print(first_name, last_name, phone_num, email, address)
 			
-			# email = row[2]		
-			
-			# if email in customers:
-			# 	continue
-			# else:
-			# 	customers[email] = {
-			# 		"First Name": first_name,
-			# 		"Last Name": last_name,
-			# 		"Email": email,
-				
-			# 	}
-			# line_count += 1
+		
 
 
 
